8e38fb112a67239b081ddee9be831c96c2986a09
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-11-04 23:46:25 +0600

    images data add

---
+++ public/data/images.js
@@ -0,0 +1,15 @@
+const images = [
+  { id: 1, thumbnail: "/assets/images/image-1.webp"},
+  { id: 2, thumbnail: "/images/assets/image-2.webp" },
+  { id: 3, thumbnail: "/images/assets/image-3.webp" },
+  { id: 4, thumbnail: "/images/assets/image-4.webp" },
+  { id: 5, thumbnail: "/images/assets/image-5.webp" },
+  { id: 6, thumbnail: "/images/assets/image-6.webp" },
+  { id: 7, thumbnail: "/images/assets/image-7.webp" },
+  { id: 8, thumbnail: "/images/assets/image-8.webp" },
+  { id: 9, thumbnail: "/images/assets/image-9.webp" },
+  { id: 10, thumbnail: "/images/assets/image-10.jpeg" },
+  { id: 11, thumbnail: "/images/assets/image-11.jpeg" },
+];
+
+export default images;
